07d8666bdfc24fdd1beaecf75fdc548ffe247aa4
ckashby <clay.ashby@gmail.com>
2019-02-04 13:46:32 -0800

    Init w/BS, Devise & Simple_form

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
